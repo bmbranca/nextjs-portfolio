d6d06fdc8422700cd1e0766346625a82d0d1180d
Brian Branca <brianbranca16@gmail.com>
2022-10-28 16:49:13 -0500

    intercom

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import HomeContainer from "../public/components/HomeContainer";
 import Footer from "../public/components/Footer";
-
+import { useIntercom } from "react-use-intercom";
 import { AppContainer } from "../public/components/styled";
 
 export default function Home() {
+  const { boot } = useIntercom();
   return (
     <AppContainer>
       <HomeContainer />
+      <button onClick={() => boot()}>Boot</button>
+
       <Footer />
     </AppContainer>
   );
